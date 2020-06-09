297b87113ec766f5cbf3309ecaaadc65a7c83aa8
aashishthakur <at1948@rit.edu>
2020-06-09 03:44:06 -0400

    create assests

--- source.cpp
+++ source.cpp
@@ -1,7 +1,52 @@
 #include <iostream>
+
 using namespace std;
-wstring  tetromino[7];
+wstring tetromino[7];
+
+int Rotate(int px, int py, int angle) {
+    switch (angle % 4) {
+        case 0:
+            return py * 4 + px;
+        case 1:
+            return 12 + py - (px * 4);
+        case 2:
+            return 15 - (py * 4) - px;
+        case 3:
+            return 3 - py + (px * 4);
+    }
+    return 0;
+}
+
+
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+
+    //create assets
+    tetromino[0].append(L"..X.");
+    tetromino[0].append(L"..X.");
+    tetromino[0].append(L"..X.");
+    tetromino[0].append(L"..X.");
+
+    tetromino[1].append(L"..X.");
+    tetromino[1].append(L".XX.");
+    tetromino[1].append(L".X..");
+    tetromino[1].append(L"....");
+
+    tetromino[2].append(L".X..");
+    tetromino[2].append(L".XX.");
+    tetromino[2].append(L"..X.");
+    tetromino[2].append(L"....");
+
+    tetromino[3].append(L"....");
+    tetromino[3].append(L".XX.");
+    tetromino[3].append(L".XX.");
+    tetromino[3].append(L"....");
+
+    tetromino[4].append(L"..X.");
+    tetromino[4].append(L".XX.");
+    tetromino[4].append(L".. X.");
+    tetromino[4].append(L"....");
+
+    tetromino[5].append(L".X...X...XX.....");
+    tetromino[6].append(L"..X...X..XX.....");
     return 0;
 }
